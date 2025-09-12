8c22381bb43f900e3707bfdaf341860e0c7125df
Петр <oc611164@gmail.com>
2025-09-12 15:48:47 +0300

    fix(page.js): use absolute paths (@) for component imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// nextjs/next.config.mjs
+import path from 'path'; // Импортируем модуль path
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Указываем корень проекта явно
+  turbopack: {
+    root: path.join(__dirname), // __dirname - это путь к папке, где лежит этот файл (корень проекта)
+  },
+  // Другие настройки, если есть...
+};
 
 export default nextConfig;
