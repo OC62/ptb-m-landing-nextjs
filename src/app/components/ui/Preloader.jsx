b7aeb66dfe982fe9dd41a4ddc6acfbe3e1c01f03
Петр <oc611164@gmail.com>
2025-09-18 17:20:44 +0300

    Настройка прелоадера

--- src/app/components/ui/Preloader.jsx
+++ src/app/components/ui/Preloader.jsx
@@ -38,7 +38,7 @@ const Preloader = () => {
         audioRef.current.currentTime = 0;
       }
       document.body.classList.add('loaded');
-    }, 9000);
+    }, 13000);
 
     // Очистка
     return () => {
